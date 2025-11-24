5204809f77f33e5592df975fa2733db480af5064
Dohaof <3151456572@qq.com>
2025-11-24 14:44:18 +0800

    init

---
+++ HarmonyApp/src/test/java/com/group9/harmonyapp/HarmonyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group9.harmonyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarmonyAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
